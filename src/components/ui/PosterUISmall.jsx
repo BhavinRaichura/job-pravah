1092aba4747d0b33ad6f23eb479d7ab0d9f3aada
BhavinRaichura <bhavinraichura28@gmail.com>
2024-01-01 19:00:18 +0530

    font change in highlights

--- src/components/ui/PosterUISmall.jsx
+++ src/components/ui/PosterUISmall.jsx
@@ -9,9 +9,9 @@ const PosterUISmall = ({image, title, updatedAt, slug, createdAt}) => {
           <img className=" w-20 h-16  rounded-md " width={200} height={300} src={image? image :"/job.jpeg"} alt="image" />
         </div>
         <div className=' pl-3 w-full '>
-            <p className=" group-hover:text-gray-400   text-xs font-normal text-gray-600"> <DateFormateElement date={updatedAt} /> </p>
+            <p className=" group-hover:text-gray-400   text-xs font-normal text-gray-600 "> <DateFormateElement date={updatedAt} /> </p>
           <Link href={`/blog/${createdAt}/${slug}`}>
-            <p className="pb-2 block  text-base  font-medium leading-snug tracking-normal  antialiased cursor-pointer text-ellipsis overflow-hidden h-16 w-full  line-clamp-2 "> <span className='line-clamp-3'>{title}</span></p>
+            <p className="pb-2 block  text-sm  font-medium leading-snug tracking-normal  antialiased cursor-pointer text-ellipsis overflow-hidden h-16 w-full  line-clamp-2 uppercase"> <span className='line-clamp-3'>{title}</span></p>
           </Link>
           
            
