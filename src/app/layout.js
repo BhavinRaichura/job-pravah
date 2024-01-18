9df8c945b334776a5ca94cc4d91f0180d5eab0f8
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2024-01-18 17:26:41 +0530

    Google analytics added: layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,17 @@ const Layout = ({children}) => {
         <head>
           <link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Jost  ' />
         </head>
+      
+        {/* Google analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5EL2KHZCVK"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-5EL2KHZCVK');
+        </script>
+
 
         <body>
           <Provider >
